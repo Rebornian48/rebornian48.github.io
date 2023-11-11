8770bc921c1abdb44524cce5f17199c8c93b4d4d
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2023-11-11 23:02:50 +0700

    Waiting Room Version 1.1.0
    Penambahan Fitur Ubah Pesan pada Waiting Room

--- waittime/script.js
+++ waittime/script.js
@@ -5,7 +5,6 @@ document.getElementById("last-updated").innerText = date.toLocaleDateString("id-
 function countdownTimer() {
     const difference = +new Date("2024-01-01") - +new Date();
     let remaining = "Selamat Tahun Baru 2024";
-
     if (difference > 0) {
         const parts = {
         hari: Math.floor(difference / (1000 * 60 * 60 * 24)),
@@ -13,13 +12,31 @@ function countdownTimer() {
         menit: Math.floor((difference / 1000 / 60) % 60),
         detik: Math.floor((difference / 1000) % 60),
         };
-        remaining = "Perkiraan waktu tunggu : " + Object.keys(parts).map(part => {
+        remaining = Object.keys(parts).map(part => {
         return `${parts[part]} ${part}`;  
         }).join(" ");
     }
-
     document.getElementById("waitTime").innerHTML = remaining;
 }
-
 countdownTimer();
-setInterval(countdownTimer, 1000);
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("change");
+var kirim = document.getElementById("kirim");
+
+btn.onclick = function () {
+    pesan.style.display = 'block';
+    kirim.style.display = 'block';
+    btn.style.display = 'none';
+}
+
+kirim.addEventListener('click', () => {
+    var pesan = document.getElementById("pesan");
+    var wait = document.getElementById("waitTime");
+    var newText = pesan.value;
+    wait.innerText = newText;
+    function countdownTimer(){};
+    pesan.style.display = 'none';
+    kirim.style.display = 'none';
+    btn.style.display = 'block';
+})
