f302e6cd3987fd40abe206f633d28d5314c97aa1
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-01-07 15:28:45 +0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,7 @@
 // Scripts
 //
 
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", () => {
   // Navbar shrink function
   var navbarShrink = function () {
     const navbarCollapsible = document.body.querySelector("#mainNav");
